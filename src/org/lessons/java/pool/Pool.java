c0ddddcae152bc53567f4841ab2aa70d8a393fe7
Alaikum <carangiadamo@gmail.com>
2023-01-25 16:58:46 +0100

    Add control and decimal format

--- src/org/lessons/java/pool/Pool.java
+++ src/org/lessons/java/pool/Pool.java
@@ -1,8 +1,10 @@
 package org.lessons.java.pool;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Pool {
+	private static final DecimalFormat df = new DecimalFormat("0.00");
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -20,28 +22,50 @@ public class Pool {
 		System.out.println("Questo è un semplice sondaggio.\n"
 				+ "Vogliamo sapere le prefereze dei tuoi studenti sui tre sistemi operativi più famosi.\n"
 				+ "Un voto a studente tra Linux, Windows e Mac OS.");
-		// inserire il numero di studenti
 		do {
+
+			// inserire il numero di studenti
 			System.out.print("Quanti studenti hai? ");
+			// controllo se è un numero
+			while (!s.hasNextInt()) {
+				System.out.print("Il numero degli studenti va inserito come cifra: ");
+				s.next();
+			}
+
 			students = s.nextInt();
 
 			// inserire il numero di studenti che usano Linux
 			System.out.print("Quanti studenti usano Linux? ");
+			// controllo se è un numero
+			while (!s.hasNextInt()) {
+				System.out.print("Il numero degli studenti che ha scelto Linux va inserito come cifra: ");
+				s.next();
+			}
 			linux = s.nextInt();
 
 			// inserire il numero di studenti che usano windows
 			System.out.print("Quanti studenti usano Windows? ");
+			// controllo se è un numero
+			while (!s.hasNextInt()) {
+				System.out.print("Il numero degli studenti che ha scelto Windows va inserito come cifra: ");
+				s.next();
+			}
 			windows = s.nextInt();
 
 			// inserire il numero di studenti che usano mac
 			System.out.print("Quanti studenti usano Mac OS? ");
+			// controllo se è un numero
+			while (!s.hasNextInt()) {
+				System.out.print("Il numero degli studenti che ha scelto Mac-OS va inserito come cifra: ");
+				s.next();
+			}
 			mac = s.nextInt();
 			totalVotes = linux + windows + mac;
 
 			// controllo voti <= agli studenti
 			if (students < totalVotes) {
 				System.out.println("ATTENZIONE!!! Hai il numero di voti è superiore al numero di studenti");
-			
+
 			}
 
 		} while (students < totalVotes); // ripete se hai troppi voti
@@ -53,17 +77,21 @@ public class Pool {
 			abstain = true;
 		}
 
-		System.out.println("Ecco i risultati, i tuoi studenti sono: " + students + ", i votanti sono " + totalVotes);
+		System.out.println("Ecco i risultati, i tuoi studenti sono: " + students + ", i votanti sono " + totalVotes+".");
 		System.out.println("Di " + totalVotes + " voti " + linux + " sono per Linux, prescisamente il "
-				+ ((linux * 1.0 / totalVotes) * 100) + "% dei votanti, o il "+((linux * 1.0 / students) * 100)+"% degli studenti");
+				+ df.format((linux * 1.0 / totalVotes) * 100) + "% dei votanti, o il "
+				+ df.format((linux * 1.0 / students) * 100) + "% degli studenti");
 		System.out.println("Di " + totalVotes + " voti " + windows + " sono per Windows, prescisamente il "
-				+ ((windows * 1.0 / totalVotes) * 100) + "% dei votanti, o il "+((windows * 1.0 / students) * 100)+"% degli studenti");
+				+ df.format((windows * 1.0 / totalVotes) * 100) + "% dei votanti, o il "
+				+ df.format((windows * 1.0 / students) * 100) + "% degli studenti");
 		System.out.println("Di " + totalVotes + " voti " + mac + " sono per Mac OS, prescisamente il "
-				+ ((mac * 1.0 / totalVotes) * 100) + "% dei votanti, o il "+((mac * 1.0 / students) * 100)+"% degli studenti");
-	
+				+ df.format((mac * 1.0 / totalVotes) * 100) + "% dei votanti, o il "
+				+ df.format((mac * 1.0 / students) * 100) + "% degli studenti");
+
 		if (abstain) {
-			System.out.println("Di " + students + " studenti " + (students - totalVotes)
-					+ " non hanno votato!!!Prescisamente il " + (((students - totalVotes) * 1.0 / students) * 100) + "% degli studenti");
+			System.out.println(
+					"Di " + students + " studenti " + (students - totalVotes) + " non hanno votato!!!Prescisamente il "
+							+ df.format(((students - totalVotes) * 1.0 / students) * 100) + "% degli studenti.");
 		}
 	}
 
